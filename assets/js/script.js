454a993bad182c0799a364fb6f6c5e864642bafe
oscar101196 <oscar101196@gmail.com>
2024-11-11 22:19:50 -0300

    Tenemos que generar apis, ya esta los botones

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ let sumarItem = () =>{
     cantidad = document.getElementById("cantidad").innerHTML;
     cantidad++;
     document.getElementById("cantidad").innerHTML = cantidad;
+
+    modificarPrecio();
 };
 
 
@@ -11,9 +13,20 @@ let restarItem = () =>{
         cantidad--
     }
     document.getElementById("cantidad").innerHTML = cantidad;
+
+    modificarPrecio();
 };
 
+let modificarPrecio = () =>{
+    cantidad = document.getElementById("cantidad").innerHTML;
+    precio = document.getElementById("precio-unidad").innerHTML;
+    precioTotal = precio * cantidad;
+    document.getElementById("precio-total").innerHTML = precioTotal;
+}
 
+let agregarProducto = () =>{
+    
+}
 
 document.getElementById("btn-+").addEventListener("click", sumarItem);
 document.getElementById("btn--").addEventListener("click", restarItem);
