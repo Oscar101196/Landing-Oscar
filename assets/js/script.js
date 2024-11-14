1f8ccd3ea4ad996fe4e1c2b08593d9380a8c381c
Oscar <oscar101196@gmail.com>
2024-11-14 13:58:18 -0300

    Agregando API pero aparece undefinid cuando la caargamos

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,27 @@ let agregarProducto = () =>{
 
 document.getElementById("btn-+").addEventListener("click", sumarItem);
 document.getElementById("btn--").addEventListener("click", restarItem);
+
+fetch('productos.json')
+    .then(response => response.json())
+    .then(date => {
+        let listaProductosDiv = document.getElementById("productos")
+
+        date.forEach(element => {
+            let productoElemento = document.createElement("figure");
+            productoElemento.classList.add("col");
+            productoElemento.innerHTML = 
+            `
+            <div class="card h-100">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <figcaption class="card-title">${date.nombre}</figcaption>
+                    <p>$${date.precio}</p>
+                    <p>10% de descuento con Transferencia bancaria</p>
+                    <button class="agregar-Carrito">Comprar</button>
+                </div>
+            </div>
+            `
+            listaProductosDiv.appendChild(productoElemento);
+        });
+    })
