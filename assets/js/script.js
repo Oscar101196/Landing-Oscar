63327c13a35ce8ee2ace917f3d2e8c8cb7a936f9
oscar101196 <oscar101196@gmail.com>
2024-11-27 21:09:22 -0300

    falta morificar el descuento por 21mil

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,5 @@
-let sumarItem = () =>{
-    cantidad = document.getElementById("cantidad").innerHTML;
-    cantidad++;
-    document.getElementById("cantidad").innerHTML = cantidad;
-
-    modificarPrecio();
-};
-
-
-let restarItem = () =>{
-    cantidad = document.getElementById("cantidad").innerHTML;
-    if(cantidad > 1){
-        cantidad--
-    }
-    document.getElementById("cantidad").innerHTML = cantidad;
-
-    modificarPrecio();
-};
+let carrito = [];
+let productosCargados = [];
 
 let modificarPrecio = () =>{
     cantidad = document.getElementById("cantidad").innerHTML;
@@ -24,93 +8,227 @@ let modificarPrecio = () =>{
     document.getElementById("precio-total").innerHTML = precioTotal;
 }
 
-let carrito = [];
+async function cargarProductos() {
+    const response = await fetch("/productos.json");
+    productosCargados = await response.json();
+    cargarCatalogoCarrito(productosCargados);
 
-
-
-let agregarCarrito = (id,talle,color) =>{
-    let productoExiste = carrito.find(elemento.id === id);
-    if((productoExiste) && (productoExiste.talle === talle) && (productoExiste.color === color)){
-        productoExiste.cantidad++;
-    }
-    else{
-        carrito.push(productoExiste);
-    }
+    //cargarCatalogoCarrito()  CONSULTAR PORQUE SOLO PUEDO USAR PRODUCTOSCARGADOS EN LA PRIMERA FUNCION, PERO EN LA SEGUNDA TENGO QUE VOLVER A USAR FETCH
 }
 
-let actualizarCarrito = () =>{
-    let carritoDiv = document.getElementById("producto-carrito");
-    carritoDiv.innerHTML = "";
-    
-    carrito.forEach((producto, index) => {
-        //Fila para cada producto
-        productoElemento = document.createElement("figure")
-        productoElemento.classList.add("row");
-        productoElemento.innerHTML = 
+function cargarCatalogoCarrito(productosCargados){
+    const listaProductosDiv = document.getElementById("productos-carrito");
+    listaProductosDiv.innerHTML = "";
+    productosCargados.forEach(
+        element => {
+        let productoHTML = 
         `
-        <div class="col-2">
-            <img src="${producto.imagenes[0]}" class=" img-thumbnail img-fluid" style="width: 140px; height: auto;" alt="">
-        </div>
-        <div class="col-10">
-            <div>
-                <div class="d-flex justify-content-between">
-                    <p>${producto.nombre}</p>
-                    <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-trash3" viewBox="0 0 16 16">
-                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-                        </svg>
-                    </button>
-                </div>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group me-2 w-25" role="group" aria-label="Second group">   
-                        <button type="button" id="btn--" class="btn btn-secondary">-</button>
-                        <p id="cantidad">${producto.cantidad}</p>
-                        <button type="button"  id="btn-+" class="btn btn-secondary">+</button>
-                    </div>
-                <div>
-                    <p>$ <span id="precio-unidad">${producto.precio}</span>  U.</p>
-                </div>
-                <div class="text-end">
-                    <p>$ <span id="precio-total">${producto.precio}*${producto.cantidad}</span></p>
-                </div>
-            </div>
-        </div>
-        `;
-        carritoDiv.appendChild(productoElemento);
-    });
-}
-
-
-
-fetch('productos.json')
-    .then(response => response.json())
-    .then(date => {
-        /*CARGA PRODUCTOS EN HOME*/
-        let listaProductosDiv = document.getElementById("productos")
-        let primerosCuatro = date.slice(0,4);
-        primerosCuatro.forEach(element => {
-            let productoElemento = document.createElement("figure");
-            productoElemento.classList.add("col");
-            productoElemento.innerHTML = 
-            `
+        <figure class "col">
             <div class="card h-100">
                 <img src="${element.imagenes[0]}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <figcaption class="card-title">${element.nombre}</figcaption>
                     <p>$${element.precio}</p>
                     <p>10% de descuento con Transferencia bancaria</p>
-                    <button class="agregar-Carrito">Comprar</button>
+                    <button onclick = "agregarCarrito(${element.id})">Comprar</button>
                 </div>
             </div>
+        </figure>
+        `;
+        listaProductosDiv.innerHTML +=productoHTML;
+        }
+    )
+}
+
+function ordenarProductos(){
+    let criterio = document.getElementById("ordenarProductos").value;
+    let productosOrdenados = [...productosCargados];
+    if(criterio === "menor-mayor"){
+        productosOrdenados.sort((a,b) => a.precio - b.precio);
+    }
+    else if(criterio === "mayor-menor"){
+        productosOrdenados.sort((a,b) => b.precio - a.precio);
+    }
+    else if(criterio === "alfabetico"){
+        productosOrdenados.sort((a,b) => a.nombre.localeCompare(b.nombre));
+    }
+    cargarCatalogoCarrito(productosOrdenados);
+}
+
+// async function cargarCatalogoHome(){
+//     const response = await fetch("/productos.json");
+//     const productosCargados2 = await response.json();
+//     let listaProductosDiv = document.getElementById("productos-home")
+//     let primerosCuatro = productosCargados2.slice(0,4);
+//     listaProductosDiv.innerHTML = "";
+//     primerosCuatro.forEach(element => {
+//         listaProductosDiv.innerHTML += 
+//         `
+//         <figure class = "col">
+//             <div class="card h-100">
+//                 <img src="${element.imagenes[0]}" class="card-img-top" alt="...">
+//                 <div class="card-body">
+//                     <figcaption class="card-title">${element.nombre}</figcaption>
+//                     <p>$${element.precio}</p>
+//                     <p>10% de descuento con Transferencia bancaria</p>
+//                     <button onclick = "agregarCarrito(${element.id}, ${element.talle})">Comprar</button>
+//                 </div>
+//             </div>
+//         </figure>
+//         `  
+//     });
+// }
+
+async function agregarCarrito(id){
+    let response = await fetch("/productos.json");
+    let productos = await response.json();
+    let cantidad = 1;
+    let productoSeleccionado = productos.find(elemento => elemento.id === id);
+    let productoExiste = carrito.find(p => p.id === id);
+    if(productoExiste){
+        productoExiste.cantidad++;
+    }
+    else{
+        let productoEnCarrito ={
+            ...productoSeleccionado,cantidad
+        }
+        carrito.push(productoEnCarrito);
+        alert(`Agregaste ${productoSeleccionado.nombre} al carrito!`);
+    }
+    actualizarCarrito();
+}
+
+
+
+// function actualizarCantidad(idProducto, cambio){
+//     let cantidadInput = document.getElementById(`cantidad-${idProducto}`);
+//     let cantidad = parseInt(cantidadInput.value);
+//     cantidad = Math.max(1,cambio+cantidad);
+//     cantidadInput.value = cantidad;
+//     console.log("entro a actualizarcantidad" + carrito);
+    
+// }
+
+function cambiarCantidad(index, cambio){
+    carrito[index].cantidad = Math.max(1,carrito[index].cantidad + cambio);
+    actualizarCarrito();
+    console.log(carrito);
+    
+}
+
+function eliminarDelCarrito(index){
+    carrito.splice(index,1);
+    actualizarCarrito();
+}
+
+function mostrarTotales(){
+    let zonaMontos = document.getElementById("montos");
+    let total = carrito.reduce((total,producto) =>{
+        return total + producto.precio * producto.cantidad},0);
+    zonaMontos.innerHTML =
+    `
+    <div> 
+        <p class="text-center">Estas a $21.000 de envio gratis</p>
+    </div>
+    <div class="row d-flex justify-content-between">
+        <div class="col-6">
+            <p>Subtotal (sin envio): </p>
+        </div>
+        <div class="col-6 text-end">
+            <p>${total}</p>
+        </div>
+    </div>
+    <div> 
+        <div class="accordion" id="accordionPanelsStayOpenExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo"                 aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                        Medios de envio
+                    </button>
+                </h2>
+                <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
+                    <div class="accordion-body">
+                        <p>kaka</p>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                    Nuestros Locales
+                    </button>
+                </h2>
+                <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
+                    <div class="accordion-body">
+                        <p>kaka</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row d-flex justify-content-between">
+        <div class="col-6"> 
+            <p>Total:</p>
+        </div>
+        <div class="col-6 text-end"><p>${total}</p></div>
+    </div>
+    <div class="d-flex justify-content-center">
+        <div>
+            <button class="btn btn-primary" type="button">INICIAR COMPRA</button>
+            <p class="text-center" data-bs-dismiss="offcanvas">Ver más productos</p>
+        </div>
+    </div>
+    `
+}
+
+let actualizarCarrito = () =>{
+    let carritoDiv = document.getElementById("producto-carrito");
+    carritoDiv.innerHTML = '';
+    if(carrito.length === 0){
+        carritoDiv.innerHTML = '<p>Tu carrito esta vacio</p>';
+    }
+    else{
+        carrito.forEach((producto, index) => {
+            //Fila para cada producto
+            carritoDiv.innerHTML +=
             `
-            listaProductosDiv.appendChild(productoElemento);
+            <figure class = "row">     
+                <div class="col-2">
+                    <img src="${producto.imagenes[0]}" class=" img-thumbnail img-fluid" style="width: 140px; height: auto;" alt="">
+                </div>
+                <div class="col-10">
+                    <div>
+                        <div class="d-flex justify-content-between">
+                            <p>${producto.nombre}</p>
+                            <button onclick = "eliminarDelCarrito(${index})">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-trash3" viewBox="0 0 16 16">
+                                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                                </svg>
+                            </button>
+                        </div>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group me-2 w-25" role="group" aria-label="Second group">  
+                                <button onclick = "cambiarCantidad(${index},-1)" type="button" id="btn--" class="btn btn-secondary">-</button>
+                                <input type = "number" value = "${producto.cantidad}" min ="1" id="cantidad-${producto.id}">
+                                <button onclick = "cambiarCantidad(${index},1)" type="button"  id="btn-+" class="btn btn-secondary">+</button>
+                            </div>
+                        <div>
+                            <p>$ <span id="precio-unidad">${producto.precio}</span>  U.</p>
+                        </div>
+                        <div class="text-end">
+                            <p>$ <span id="precio-total">${producto.precio*producto.cantidad}</span></p>
+                        </div>
+                    </div>
+                </div>
+            </figure>
+            `;           
         });
-    })
-    /*.catch('Ocurrio un error al cargar el inventario', error)*/
+    }
+    mostrarTotales()
+}
 
-
-
-document.getElementById("btn-+").addEventListener("click", sumarItem);
-document.getElementById("btn--").addEventListener("click", restarItem);
-
-agregarCarrito();
+// document.addEventListener("click", cambiarCantidad);
+// document.addEventListener("click", cambiarCantidad);
+document.addEventListener("change", ordenarProductos); //POR ALGUNA RAZON, LA FUNCION ONCHAGE EN EL HTML NO ME LO TOMABA, ESTABA BIEN ESCRITO Y DEMAS.
+window.onload = cargarProductos;
+actualizarCarrito();
